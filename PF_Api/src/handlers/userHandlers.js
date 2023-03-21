28ce1c1cbe9dcb95769546d4a01862b61a82ed89
delcas <delcas1111@gmail.com>
2023-03-21 17:45:11 -0500

    correccion errores git

---
+++ PF_Api/src/handlers/userHandlers.js
@@ -0,0 +1,7 @@
+const {} = require("../controllers/userControllers.js");
+
+module.exports = {
+  postUserHandler: async (req, res) => {},
+  getUsersHandler: async (req, res) => {},
+  getAUserHandler: async (req, res) => {},
+};
