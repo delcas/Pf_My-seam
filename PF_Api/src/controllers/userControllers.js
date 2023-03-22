67cbd6ff819ca6b34d915ee1d03d68ba290487a1
Alba Tyto <gperettig@gmail.com>
2023-03-22 19:38:30 -0300

    feat(PF_api/controllers): add module exports
    user:creator,getters
    product: creator, getters
    service: creator, getters
    just structure, not logic

--- PF_Api/src/controllers/userControllers.js
+++ PF_Api/src/controllers/userControllers.js
@@ -1,1 +1,14 @@
 const { User } = require('../db.js');
+
+
+module.exports = {
+    userCreator: async () => {
+        await User.create()
+    },
+    getAUser: async () => {
+        await User.findByPk()
+    },
+    getUsers: async () => {
+        await User.findAll()
+    },
+}
