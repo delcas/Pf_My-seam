8a92e47ae76d2fbe1c2d5ff8181f11a32ba60594
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-03-22 21:35:57 -0300

    main.jsx recodeado

--- PF_Client/src/main.jsx
+++ PF_Client/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <Provider store={store}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>    
+    </Provider>
   </React.StrictMode>,
 )
