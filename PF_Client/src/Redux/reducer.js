1558d5b05ae93e89a8ae1dc91cc7f8a319858088
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-03-22 23:20:55 -0300

    Basic reducer created

---
+++ PF_Client/src/Redux/reducer.js
@@ -0,0 +1,23 @@
+import {GET_PRODUCTS,    
+        GET_SERVICES, 
+        } from "./actions";
+
+const initialState = {
+    products: [],
+    services: [],    
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type){
+        case GET_PRODUCTS:
+            return {...state, 
+                products: action.payload};  
+        case GET_SERVICES:
+            return {...state, 
+                services: action.payload};  
+        default:
+            return {...state};
+    }
+};
+
+export default rootReducer
