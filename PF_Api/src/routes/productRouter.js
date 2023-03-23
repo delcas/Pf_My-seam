d8f99828b8511ef53d32a661b193c74cbe45bbd3
delcas <delcas1111@gmail.com>
2023-03-22 19:28:31 -0500

    llenado tabla Products con API

--- PF_Api/src/routes/productRouter.js
+++ PF_Api/src/routes/productRouter.js
@@ -1,5 +1,19 @@
 const { Router } = require("express");
-
+const { fillTableProducts } = require("../utils/utils");
 const productRouter = Router();
+
+//llenar la tabla products
+try {
+  fillTableProducts();
+} catch (error) {
+  console.log(error.message);
+}
+//-------------------------------------
+
+productRouter.get("/", (req, res) => {});
+productRouter.get("/:idProduct", (req, res) => {});
+productRouter.post("/", (req, res) => {});
+productRouter.delete("/:idProduct", (req, res) => {});
+productRouter.put("/:idProduct", (req, res) => {});
 
 module.exports = productRouter;
