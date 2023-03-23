c205cee5778fa67b41e0d69202fa27f9ec52cf78
jatdalf <70412644+jatdalf@users.noreply.github.com>
2023-03-22 21:39:16 -0300

    main.jsx recodeado

--- PF_Client/src/main.jsx
+++ PF_Client/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
