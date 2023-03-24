4287fe8767634d447f718b9dccca9f99ee0f122c
Ignacio Waigandt <ignaawaig@gmail.com>
2023-03-23 21:05:42 -0300

    agrego serviceRouter

--- PF_Api/src/routes/index.js
+++ PF_Api/src/routes/index.js
@@ -3,10 +3,12 @@ const router = Router();
 // Importar todos los routers;
 const productRouter = require("./productRouter");
 const userRouter = require("./userRouter");
+const serviceRouter = require("./serviceRouter");
 
 // Configurar los routers
 
 router.use("/product", productRouter);
 router.use("/users", userRouter);
+router.use("/service",serviceRouter);
 
 module.exports = router;
