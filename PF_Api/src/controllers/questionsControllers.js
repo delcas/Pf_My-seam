4118380ed73799be402224335015c0c5977beef3
Alba Tyto <gperettig@gmail.com>
2023-03-25 16:30:39 -0300

    feat(Question handler and controller): create new question

--- PF_Api/src/controllers/questionsControllers.js
+++ PF_Api/src/controllers/questionsControllers.js
@@ -1,14 +1,38 @@
-const { Service } = require('../db.js');
-
+const { QuestServ, QuestProd } = require("../db.js");
 
 module.exports = {
-    serviceCreator: async () => {
-        await Service.create()
-    },
-    getAService: async () => {
-        await Service.findByPk()
-    },
-    getServices: async () => {
-        await Service.findAll()
-    },
-}
+  createQuestion: async ({ question, offertype, customerId, offerId }) => {
+    if (offertype === "service") {
+      await QuestServ.create({
+        question,
+        user_id: customerId,
+        service_id: offerId,
+      });
+    }
+    if (offertype === "product") {
+      await QuestProd.create({
+        question,
+        user_id: customerId,
+        product_id: offerId,
+      });
+    }
+  },
+  getSellerQuestions: async () => {
+    await Service.findByPk();
+  },
+  getCustomerQuestions: async () => {
+    await Service.findAll();
+  },
+  getProductQuestions: async () => {
+    await Service.findAll();
+  },
+  getServiceQuestions: async () => {
+    await Service.findAll();
+  },
+  setAnswer: async () => {
+    await Service.findAll();
+  },
+  deleteQuestion: async () => {
+    await Service.findAll();
+  },
+};
--- PF_Api/src/handlers/questionsHandlers.js
+++ PF_Api/src/handlers/questionsHandlers.js
@@ -1,22 +1,34 @@
-const { createQuestion, getSellerQuestions, getCustomerQuestions, getProductQuestions, getServiceQuestions, setAnswer, deleteQuestion} = require("../controllers/serviceControllers.js");
+const {
+  createQuestion,
+  getSellerQuestions,
+  getCustomerQuestions,
+  getProductQuestions,
+  getServiceQuestions,
+  setAnswer,
+  deleteQuestion,
+} = require("../controllers/serviceControllers.js");
 
 module.exports = {
   postQuestionsHandler: async (req, res) => {
+    const { question, offertype, customerId } = req.body;
+    const { offerId } = req.params;
     try {
-      const { question, offertype, customerId } = req.body;
-      const { offerId } = req.params;
-      console.log('Creating Question-(Handler)-');
-      await createQuestion();
-      res.status(200).send('Question sent')
+      if(!question) throw new Error('Question content missing')
+      if(!customerId) throw new Error('Customer unknown')
+      if(!offerId) throw new Error('Question unrelated to an offer')
+      if (offertype === "service" || offertype === "porduct") {
+        console.log("Creating Question -(Handler)-");
+        await createQuestion({ question, offertype, customerId, offerId });
+        res.status(201).send("Question sent");
+      } else {
+        throw new Error("Offer type is not specified correctly");
+      }
     } catch (error) {
-      
+      res.status(400).send(error.message);
     }
   },
   getQuestionsHandler: async (req, res) => {
     try {
-      
-    } catch (error) {
-      
-    }
+    } catch (error) {}
   },
 };
