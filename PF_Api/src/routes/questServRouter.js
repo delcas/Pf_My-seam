f148ca7133a8fce07b77880f9a299a73525bb97a
Alba Tyto <gperettig@gmail.com>
2023-03-26 22:51:11 -0300

    add service route put missing

--- PF_Api/src/routes/questServRouter.js
+++ PF_Api/src/routes/questServRouter.js
@@ -5,6 +5,6 @@ const questServRouter = Router();
 const prod_path = '/service'
 questServRouter.get(prod_path, getQuestionsHandler );
 questServRouter.post(`${prod_path}/:offerId`, postQuestionsHandler );
-
+questServRouter.put(`${prod_path}/:questId`, setAnswerHandler );
 
 module.exports = questServRouter;
