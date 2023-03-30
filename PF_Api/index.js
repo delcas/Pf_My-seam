b20014126c84d703e99eb532f571a8e704b1bca6
Alba Tyto <gperettig@gmail.com>
2023-03-30 10:01:07 -0300

    default preload user, service, quests

--- PF_Api/index.js
+++ PF_Api/index.js
@@ -23,7 +23,7 @@ require("dotenv").config();
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`is listening at port ${PORT}`); // eslint-disable-line no-console
   });
