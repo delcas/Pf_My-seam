ad90bb3737cbc84b8b41d695a254159d7348111e
Alba Tyto <gperettig@gmail.com>
2023-03-31 09:38:58 -0300

    feat(views/ProductDetail): Editar conditional link

--- PF_Client/src/views/ProductDetail/ProductDetail.jsx
+++ PF_Client/src/views/ProductDetail/ProductDetail.jsx
@@ -1,76 +1,100 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import style from "./ProductDetail.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { getProductById, getProductQuestions } from '../../Redux/actions'
-import { NavBar } from '../../components/NavBar/NavBar'
+import { getProductById, getProductQuestions } from "../../Redux/actions";
+import { NavBar } from "../../components/NavBar/NavBar";
 export const ProductDetail = () => {
-  const details = useSelector(state => state.details) 
+  const details = useSelector((state) => state.details);
   const dispatch = useDispatch();
-  const [question, setQuestion] = useState('')
-  const questions = details.questions 
+  const [question, setQuestion] = useState("");
+  const questions = details.questions;
+  //Variable provisoria que a futuro deberá llegar desde el estado global
+  const [userId, setuserId] = useState(1);
 
-
-  useEffect(()=>{    
-    const urlID = (window.location.href)
-    let prodID =urlID.split('/')
+  useEffect(() => {
+    const urlID = window.location.href;
+    let prodID = urlID.split("/");
     // eslint-disable-next-line
-    dispatch(getProductById(prodID[prodID.length -1]));   
-    // dispatch(getProductById(id)); 
-    getProductQuestions()
-  },[dispatch])  
+    dispatch(getProductById(prodID[prodID.length - 1]));
+    // dispatch(getProductById(id));
+    getProductQuestions();
+  }, [dispatch]);
 
   function onChange(event) {
-    if(event !== '-') {
-      setQuestion(questions.find(q => q.name === event))       
+    if (event !== "-") {
+      setQuestion(questions.find((q) => q.name === event));
     } else {
-    setActivity('')
+      setActivity("");
     }
-  }    
-  
+  }
+
   return (
     <div>
       <NavBar />
-      { 
-      details.length !== 0 ?
-      <div>
-        <h1> Detalle del producto </h1>
-        <table className={style.detailTable}>
-        <tr>                
-          <td className={style.box} rowSpan="6">
-              <img className={style.FlagImg} src={details.image[0]} alt={`imagen del producto ${details.name}`} />
-            </td>
-            <td className={style.tdLeft}>
-              <tr>Producto: {details.name}</tr>   
-              <tr>Descripcion: {details.description}</tr>  
-              <tr>Precio: {details.price}</tr>  
-              <tr>Stock: {details.stock}</tr> 
-              <tr>Vendedor: {details.userId}</tr> 
-              <tr>Disponible: NYI</tr>                 
-          </td>
-          </tr>
+      {details.length !== 0 ? (
+        <div>
+          <h1> Detalle del producto </h1>
+          <table className={style.detailTable}>
+            <tr>
+              <td className={style.box} rowSpan="6">
+                <img
+                  className={style.FlagImg}
+                  src={details.image[0]}
+                  alt={`imagen del producto ${details.name}`}
+                />
+              </td>
+              <td className={style.tdLeft}>
+                {userId === details.userid ? <th>Editar 🖊
+            </th> : ''}
+                <tr>Producto: {details.name}</tr>
+                <tr>Descripcion: {details.description}</tr>
+                <tr>Precio: {details.price}</tr>
+                <tr>Stock: {details.stock}</tr>
+                <tr>Vendedor: {details.userid}</tr>
+                <tr>Disponible: NYI</tr>
+              </td>
+            </tr>
           </table>
           <table className={style.detailTable}>
-          <tr><th colSpan="2">Preguntas:</th></tr>             
             
-              <select onChange={(e) => {onChange(e.target.value)}}>
-              <option value='-'>ver preguntas</option>
-              {questions && questions.map((q) => (
-                <option value={`${q.id}`} key={`${q.id}`}>{q.id}</option>
-              ))}
-              </select>
-            
-              { question ? (
-                  <>
-                      <tr><td>Pregunta: </td><td>{question.question}</td></tr>
-                      <tr><td>Respuesta: </td><td>{question.answer}</td></tr>                    
-                  </> ) : ''
-              }
-                                  
-        </table>
-      </div>
-        : 'No se encontró el ID'
-      }
+            <tr>
+              
+              <th colSpan="2">Preguntas:</th>
+            </tr>
 
+            <select
+              onChange={(e) => {
+                onChange(e.target.value);
+              }}
+            >
+              <option value="-">ver preguntas</option>
+              {questions &&
+                questions.map((q) => (
+                  <option value={`${q.id}`} key={`${q.id}`}>
+                    {q.id}
+                  </option>
+                ))}
+            </select>
+
+            {question ? (
+              <>
+                <tr>
+                  <td>Pregunta: </td>
+                  <td>{question.question}</td>
+                </tr>
+                <tr>
+                  <td>Respuesta: </td>
+                  <td>{question.answer}</td>
+                </tr>
+              </>
+            ) : (
+              ""
+            )}
+          </table>
+        </div>
+      ) : (
+        "No se encontró el ID"
+      )}
     </div>
-  )
-}
+  );
+};
