0a5ab66afe0f2fd5d2bbcfaeb5f22b03e134313b
delcas <delcas1111@gmail.com>
2023-04-01 11:47:00 -0500

    profile

--- PF_Client/src/components/Auth0/Profile/Profile.jsx
+++ PF_Client/src/components/Auth0/Profile/Profile.jsx
@@ -1,21 +1,59 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { NavBar } from '../../NavBar/NavBar'
+import { NavBar } from "../../NavBar/NavBar";
+import { Footer } from "../../Footer/Footer";
+// chakra
+import { Image } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <Spinner color="red.500" size="xl" />;
   }
 
   return (
     isAuthenticated && (
       <div>
         <NavBar />
-        <img src={user.picture} alt={user.name} />
-        <h2>Nombre: {user.name}</h2>
-        <p>Email: {user.email}</p>
-        <p>Nombre de usuario: {user.nickname}</p>
+        <Box>
+          <Box
+            borderWidth="3px"
+            borderRadius="100px"
+            px={40}
+            display="flex"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            alignItems="center"
+            margin="50px"
+            // alignItems="baseline"
+          >
+            <Box marginTop="20px" marginBottom="20px">
+              <Image
+                borderRadius="full"
+                boxSize="300px"
+                fallbackSrc="https://res.cloudinary.com/dp9kh0hqs/image/upload/v1680229136/logo_Seam-400x400_gmdbvq.png"
+                src={user.picture}
+                alt={user.name}
+              />
+            </Box>
+            <Box fontSize="xl" ml="2" px={100} as="h4">
+              {/* <img src={user.picture} alt={user.name} /> */}
+              <Text as="b" fontSize="3xl">
+                Nombre de usuario: {user.nickname}
+              </Text>
+              <Text></Text>
+              <Text>Nombre: {user.name}</Text>
+              <Text>Email: {user.email}</Text>
+              <Text>Fecha de acimiento: {user.email}</Text>
+              <Text>País: {user.email}</Text>
+              <Text>Ciudad: {user.email}</Text>
+              <Text>Dirección: {user.email}</Text>
+            </Box>
+          </Box>
+        </Box>
       </div>
     )
   );
