946557e65a9ec4dfc19fba1ba05677299f2bc96b
Ignacio Waigandt <ignaawaig@gmail.com>
2023-04-01 11:46:46 -0300

    mejoras a los servicios y errores

--- PF_Api/src/controllers/serviceControllers.js
+++ PF_Api/src/controllers/serviceControllers.js
@@ -6,8 +6,9 @@ module.exports = {
   serviceCreator: async (data) => {
     const { name, description, price, userid } = data;
     // imagen dummy
-    const image =
-      ["https://www.objetivobienestar.com/uploads/s1/18/19/76/6/la-importancia-y-los-beneficios-de-la-costura.jpeg"];
+    const image = [
+      "https://www.objetivobienestar.com/uploads/s1/18/19/76/6/la-importancia-y-los-beneficios-de-la-costura.jpeg",
+    ];
     // imagen dummy
     if (price <= 0) throw new Error("El precio debe ser mayor a 0");
     return await Service.create({
@@ -22,6 +23,7 @@ module.exports = {
     const service = await Service.findByPk(id, {
       include: User,
     });
+    if (!service) throw new Error(`id ${id} not found`);
     return service;
   },
   getServices: async (name) => {
@@ -30,12 +32,12 @@ module.exports = {
         async (serv) =>
           await Service.findOrCreate({
             where: {
-            name: serv.name,
-            description: serv.description,
-            price: serv.price,
-            image: serv.image,
-            userid: serv.userid,
-            }
+              name: serv.name,
+              description: serv.description,
+              price: serv.price,
+              image: serv.image,
+              userid: serv.userid,
+            },
           })
       );
       return await Service.findAll();
@@ -51,28 +53,24 @@ module.exports = {
     return services;
   },
   deleteService: async (id) => {
-    try {
-      const service = await Service.destroy({
+    const service = await Service.destroy({
+      where: {
+        id: id,
+      },
+    });
+    if (!service) throw new Error(`id ${id} not found`);
+    return `id ${id} successfully deleted`;
+  },
+  updateService: async (id, data) => {
+    const { name, description, price } = data;
+    const service = await Service.update(
+      { name, description, price },
+      {
         where: {
           id: id,
         },
-      });
-      if(!service) throw new Error(`id ${id} not found`);
-    } catch (error) {
-      return { error: error };
-    }
-  },
-  updateService: async (id, data) => {
-    const { name, description, price } = data
-    try{
-      const service = await Service.update({ name, description, price },{
-        where: {
-          id: id
-        }
-      })
-    }
-    catch (error) {
-      return { error: error };
-    }
+      }
+    );
+    if (!service[0]) throw new Error(`id ${id} not found`);
   },
 };
