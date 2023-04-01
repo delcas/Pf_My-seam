0d58ba8cc552ce5912d6701372403e9781839e46
Jorge Toso <jorgetoso@hotmail.com>
2023-04-01 00:15:35 -0300

    Landing & Detail refactor

--- PF_Client/src/views/Landing/Landing.jsx
+++ PF_Client/src/views/Landing/Landing.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styles from './Landing.module.css'
+import BackgroundVideo from "../../assets/images/MySeamVideo.mp4"
 
 export const Landing = () => {
   return (
     <div className={styles.containerLanding}>
+      <video className={styles.landingVideo} autoPlay muted id="background-video">
+        <source src={BackgroundVideo} type="video/mp4" />
+      </video>
+
       <Link to='/home'>
         <button className={styles.buttonToHome}>Go HomePage</button>
       </Link>
