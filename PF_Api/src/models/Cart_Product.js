a5322d955d09b3e70a977921ae2bbb3abbc925f1
Alba Tyto <gperettig@gmail.com>
2023-04-02 18:26:32 -0300

    feat(PF_Api/src.. /model/Cart_Product.js): create model Cart_Product to add quantity

---
+++ PF_Api/src/models/Cart_Product.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+const Cart = require("./Cart");
+const Product = require("./Product");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "cart_product",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      cartid: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Cart,
+          key: "id",
+        },
+        allowNull: false,
+      },
+      productid: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: Product,
+          key: "id",
+        },
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      conclusion: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
