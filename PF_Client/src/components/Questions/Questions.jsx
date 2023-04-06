6507cdd5682b1e24dbb19cb768c8c278d2df0947
Alba Tyto <gperettig@gmail.com>
2023-04-05 22:18:03 -0300

    feat(Client/src/component/Questions): add button and modal to send quest

--- PF_Client/src/components/Questions/Questions.jsx
+++ PF_Client/src/components/Questions/Questions.jsx
@@ -1,27 +1,70 @@
 import questyle from "./Questions.module.css";
 import { useSelector } from "react-redux";
 import {
+
   Box,
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
+  Button,
+  Textarea,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  ModalBody,
+  useDisclosure,
+  Modal,
+  ModalContent,
+  ModalFooter,
 } from "@chakra-ui/react";
+import { useRef } from "react";
 
 export default function Questions() {
   const productQuestions = useSelector((state) => state.productQuestions);
-
-  // const acordionHandler = (productQuestions) => {
-  //   if (productQuestions){
-  //   return
-  //   });}
-  // }
-
+  
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    const initialRef = useRef(null)
+    const finalRef = useRef(null)
+    const sendQuestHandler = ()=>{
+      
+    }
+  
   return (
     <div className={questyle.detailTable}>
       <div>
         <label colSpan="2">Preguntas:</label>
+    <span>
+      <Button onClick={onOpen}>Haz una pregunta</Button>
+            <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Tu Pregunta</FormLabel>
+              <Textarea ref={initialRef} placeholder='question' />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={sendQuestHandler}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </span>
+
       </div>
       <div>
         {productQuestions.length > 0 ? (
