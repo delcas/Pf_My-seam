eefba1ec00f60d8233de43b870a907d103b8ddc0
Alba Tyto <gperettig@gmail.com>
2023-04-05 23:21:37 -0300

    stand by(Client.../Questions.jsx): progressing send quest/ answer. Sleep pause

--- PF_Client/src/components/Questions/Questions.jsx
+++ PF_Client/src/components/Questions/Questions.jsx
@@ -1,7 +1,7 @@
 import questyle from "./Questions.module.css";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import {
-
   Box,
   Accordion,
   AccordionItem,
@@ -23,15 +23,22 @@ import {
 } from "@chakra-ui/react";
 import { useRef } from "react";
 
-export default function Questions() {
+export default function Questions(userId, details) {
   const productQuestions = useSelector((state) => state.productQuestions);
-  
+  const [error, setError] = useState({});
+  const [sndquest, setSndQuest] = useState({ customerId: userId });
+    const changeHandler = (event) => {
+      const property = event.target.name;
+      const value = event.target.value;
+      setSndQuest({ ...sndquest, [property]: value });
+    };
+
     const { isOpen, onOpen, onClose } = useDisclosure()
   
     const initialRef = useRef(null)
     const finalRef = useRef(null)
-    const sendQuestHandler = ()=>{
-      
+    const sendQuestHandler = async (ev)=>{
+      await axios.post(`/questprod/product/${pId}`, quest);
     }
   
   return (
@@ -52,14 +59,18 @@ export default function Questions() {
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Tu Pregunta</FormLabel>
-              <Textarea ref={initialRef} placeholder='question' />
+              <Textarea 
+              ref={initialRef} 
+              placeholder='question' 
+              onChange={changeHandler}
+              />
             </FormControl>
           </ModalBody>
           <ModalFooter>
             <Button colorScheme='blue' mr={3} onClick={sendQuestHandler}>
-              Save
+              Enviar
             </Button>
-            <Button onClick={onClose}>Cancel</Button>
+            <Button onClick={onClose}>Cancelar</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
