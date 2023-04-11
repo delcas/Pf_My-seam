81c38404596ba238633f9c4e271d1e57167e117f
Alba Tyto <gperettig@gmail.com>
2023-04-11 18:49:23 -0300

    feat(Cart controller): add more than one product to cart (middle table)

--- PF_Api/src/controllers/cartController.js
+++ PF_Api/src/controllers/cartController.js
@@ -1,47 +1,54 @@
-const { Cart, Product} = require("../db.js");
+const { Cart, Product, Cart_Product} = require("../db.js");
 
-module.exports={
-    postCartProduct: async({ customer_id, productid, quantity })=>{
+
+    const postCartProduct = async({ customer_id, prods })=>{
+        //customer_id, [{productid: N, quantity: N}, {productid: N, quantity: N}]
         console.log('Llega al controller');
         const new_cart = await Cart.create({
             state: "En Compra",
             customer_id,
-        });
-        const add_prod = await new_cart.addProduct(productid, {
-            through: {
-              quantity: quantity
-            }
-          });
-        return add_prod;
-    },
-    getCartProducts:
-    async()=>{
+        });        
+        return await AddProductToCart(new_cart, prods);
+    };
+    const getCartProducts = async()=>{
 
-    }, 
-    getActiveCart: async(customer_id)=>{
+    };
+    const getActiveCart = async(customer_id)=>{
         const new_cart = await Cart.findOne({
             where: {
                 customer_id,
                 state: "En Compra",
             }            
         })
-        console.log('Controller getCart: ', new_cart.toJSON());
+        if (new_cart) console.log('Controller getCart: ', new_cart.toJSON());
         return new_cart;
-    },    
+    };   
 
-    AddProductToCart: async({ active, productid, quantity })=>{
-        return await active.addProduct(productid, {
+    const AddProductToCart = async({ active, prods })=>{
+        const add_prods = prods?.map( async (p) => {
+            //buscar en tabla intermedia...
+            const prod = await Product.findByPk(p.productid);
+            if(p.quantity > prod.stock) throw new Error(`La cantidad solicitada de ${prod.name} supera el stock disponible, solo quedan ${prod.stock} unidades`);
+            return await active.addProduct(p.productid, {
             through: {
-              quantity: quantity
+              quantity: p.quantity
             }
-          }) 
-    },
+          })
+        });
+        return add_prods;
+    };
     
-    deleteCartProduct: async()=>{
+    const deleteCartProduct = async()=>{
         
-    },
-    deleteCartAllProducts:async()=>{
+    };
+    const deleteCartAllProducts = async()=>{
         
-    }
-
+    };
+module.exports={
+    getActiveCart,
+    AddProductToCart,
+    getCartProducts,
+    postCartProduct,
+    deleteCartProduct,
+    deleteCartAllProducts,
 }
--- PF_Api/src/handlers/cartHandler.js
+++ PF_Api/src/handlers/cartHandler.js
@@ -19,24 +19,28 @@ module.exports = {
 // };)
 
 createCartProduct: async (req, res) => {
-    const { customer_id, productid, quantity } = req.body;
-    console.log('CartPost handler: ', customer_id, productid, quantity );
+  
+  //se puede hacer que en cuenta de productid y quantity por separado, vengan como un array de objetos con esas dos propiedades cada uno. Para de esa forma poder recibir varios productos al crear el carrito.
+  const { customer_id, prods} = req.body; // --> Donde prods = [ productid, quantity ]
+    // const { customer_id, productid, quantity } = req.body;
+    console.log('CartPost handler: ', customer_id, prods );
+    //customer_id, [{productid: N, quantity: N}, {productid: N, quantity: N}]
   try {
     const active = await getActiveCart(customer_id);
-    const ver = active.toJSON();
+    let ver;
+    active ? ver = active.toJSON() : ver = {id:null};
     console.log(ver.id);
     if(!ver.id) {
-      const new_cart = await postCartProduct({ customer_id, productid, quantity })
+      const new_cart = await postCartProduct({ customer_id, prods })
       res.status(201).json(new_cart);
     } else {
-      const in_cart = await AddProductToCart({ active, productid, quantity });
+      const in_cart = await AddProductToCart({ active, prods });
       res.status(201).json(in_cart);
-    }
-  
+    };  
   } catch (error) {
     console.log(error.message);
     res.status(400).send(error.message);
-  }
+  };
 },
 
 // const deleteCartProduct = async (req, res) => {};
