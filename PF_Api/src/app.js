51d5180fb0dedada6bc5c7710c853c13fb758d6d
Alba Tyto <gperettig@gmail.com>
2023-07-05 09:26:40 -0300

    feat(PF_Api): conditional origin request, local or deploy

--- PF_Api/src/app.js
+++ PF_Api/src/app.js
@@ -5,27 +5,36 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const cors = require("cors");
 require("dotenv").config();
-const { ACCESS } = process.env;
+const { ACCESS1, ACCESS2 } = process.env;
 
 require("./db.js");
 
 const server = express();
+//Verificar origin:
+const originArray = [ ACCESS1, ACCESS2 ];
+console.log(originArray);
 // Configurar CORS
-server.use(
-  cors({
-    origin: ACCESS, // Cambiar esto con el origen de tu cliente
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type, Authorization, x-access-token",
-  })
-);
+
 server.name = "API";
 
+server.use((req, res, next) => {
+  const req_origin = req.headers.origin
+  const origin = originArray.find((e)=> e === req_origin) ? req_origin : null;
+  console.log(origin);
+  cors({    
+    origin: origin, // Cambiar esto con el origen de tu cliente
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization, x-access-token",
+  });
+  next();
+}
+);
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", ACCESS); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", req.headers.origin); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
